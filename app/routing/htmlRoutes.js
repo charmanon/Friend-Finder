cf6fc56acb00a3e8fa9670418a0b8fa2988c70c1
sharloteean <sharloteean@gmail.com>
2017-04-27 19:06:12 -0700

    Commented in the api routing

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -15,16 +15,16 @@ module.exports = function(app) {
   // In each of the below cases the user is shown an HTML page of content
   // ---------------------------------------------------------------------------
 
-  // app.get("/home", function(req, res) {
-  //   res.sendFile(path.join(__dirname, "/../public/home.html"));
-  // });
+  app.get("/home", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/home.html"));
+  });
 
-  // app.get("/survey", function(req, res) {
-  //   res.sendFile(path.join(__dirname, "/../public/survey.html"));
-  // });
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/survey.html"));
+  });
 
-  // // If no matching route is found default to home
-  // app.use(function(req, res) {
-  //   res.sendFile(path.join(__dirname, "/../public/home.html"));
-  // });
+  // If no matching route is found default to home
+  app.use(function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/home.html"));
+  });
 };
