6997ccccef8e98203d4195e07bb365c247acf174
sharloteean <sharloteean@gmail.com>
2017-04-27 19:04:41 -0700

    Added static directory to serve public files

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ var PORT = process.env.PORT || 8080;
 
 // BodyParser makes it possible for our server to interpret data sent to it.
 // The code below is pretty standard.
-app.use(express.static(path.join(__dirname ,'/public')));
+app.use('/static', express.static(path.join(__dirname, 'app/public')))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
