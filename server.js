954bfa5f4c485a1e16fdaf1ac3b5887c9abd7d2e
sharloteean <sharloteean@gmail.com>
2017-04-27 18:46:52 -0700

    Added path to server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 // DEPENDENCIES
 // Series of npm packages that we will use to give our server useful functionality
 // ==============================================================================
-
+var path = require("path");
 var express = require("express");
 var bodyParser = require("body-parser");
 
