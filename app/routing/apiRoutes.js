7570ce441b0436e570d6dc8e59b5dbb555a43178
sharloteean <sharloteean@gmail.com>
2017-04-29 00:10:53 -0700

    Added data validation that stops modal from showing up

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,43 +5,8 @@
 // ===============================================================================
 
 var friends = require("../data/friends");
+
 // var friends = require("./friends.js");
-var indexFriends = friends.length - 1;
-var bestFriendIndex = 0;
-var currentComparison = 40;
-
-//The newest user is the last item in the array
-var user = friends[indexFriends];
-
-//Goes through the index of users in array of friends
-for (var i = 0; i < indexFriends; i++){
-  // console.log(friends[i].scores);
-  var totalDifference = 0; 
-
-  //Goes through the index of scores in the users being compared
-  for (var j = 0; j < 10 ; j++){
-    
-    //Calculates the absolute value between the two array scores at each index
-    var difference = Math.abs(parseFloat(user.scores[j]) - parseFloat(friends[i].scores[j]) );
-
-    //Adds the total value of the differences
-    totalDifference = totalDifference + difference;
-  }
-
-  //After calculating the total, checks to see if the total difference is less than the current difference
-  if (totalDifference < currentComparison){
-    
-    //Stores the index of new best friend
-    bestFriendIndex = i;
-
-    //Sets lowest difference to the new number to be compared
-    currentComparison = totalDifference;
-
-    // console.log(currentComparison);
-  }
-}
-
-var bestfriend = friends[bestFriendIndex];
 // ===============================================================================
 // ROUTING
 // ===============================================================================
@@ -53,10 +18,50 @@ module.exports = function(app) {
   // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
 
-  app.get("/friends", function(req, res) {
+  app.get("/bestfriend", function(req, res) {
+    var indexFriends = friends.length - 1;
+    var bestFriendIndex = 0;
+    var currentComparison = 40;
+
+    //The newest user is the last item in the array
+    var user = friends[indexFriends];
+
+    //Goes through the index of users in array of friends
+    for (var i = 0; i < indexFriends; i++){
+      // console.log(friends[i].scores);
+      var totalDifference = 0; 
+
+      //Goes through the index of scores in the users being compared
+      for (var j = 0; j < 10 ; j++){
+        
+        //Calculates the absolute value between the two array scores at each index
+        var difference = Math.abs(parseFloat(user.scores[j]) - parseFloat(friends[i].scores[j]) );
+
+        //Adds the total value of the differences
+        totalDifference = totalDifference + difference;
+      }
+
+      //After calculating the total, checks to see if the total difference is less than the current difference
+      if (totalDifference < currentComparison){
+        
+        //Stores the index of new best friend
+        bestFriendIndex = i;
+
+        //Sets lowest difference to the new number to be compared
+        currentComparison = totalDifference;
+
+        // console.log(currentComparison);
+      }
+    }
+
+    var bestfriend = friends[bestFriendIndex];
+
     res.json(bestfriend);
   });
 
+  app.get("/friends", function(req, res) {
+    res.json(friends);
+  });
   // API POST Requests
   // Below code handles when a user submits a form and thus submits data to the server.
   // In each of the below cases, when a user submits form data (a JSON object)
